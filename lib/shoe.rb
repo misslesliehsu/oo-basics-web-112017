48ca599bb4c8539719e60f716011e9f55bfc345d
Flatiron School <leslie.hsu@flatironschool.com>
2017-11-22 10:58:01 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+#brand, color, size, material, condition
+attr_accessor :brand, :condition, :color, :size, :material
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+
+end
